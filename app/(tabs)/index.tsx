dc236098b90c69384989808604db0f715824761e
Daniel Calderon <danycalvir@gmail.com>
2025-12-23 22:55:49 -0700

    Fetch movies and create card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const { 
+    data: movies,
+    error: moviesError,
+    loading: moviesLoading
+  } = useFetch(()=> fetchMovies({
+    query:'faster'
+  }))
 
   return (
     <View className="flex-1 bg-primary">
@@ -16,14 +27,43 @@ export default function Index() {
         contentContainerStyle={{
           minHeight: "100%", paddingBottom: 10 
         }}>
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"
-        />
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"  
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        {moviesLoading ? (
+          <ActivityIndicator 
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
+        ): moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ): (
+          <View className="flex-1 mt-5">
+            <SearchBar 
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"  
+            />
+            <>
+              <Text className="text-white text-lg font-bold mt-5 mb-3">Latest Movies</Text>
+              <FlatList 
+                data={movies}
+                renderItem={({item})=>(
+                  <MovieCard {...item}/>
+                )}
+                keyExtractor={(item)=>item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: 'flex-start', 
+                  marginBottom: 10,
+                  paddingRight:5,
+                  gap:20
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
         </View>
+        )}
+        
       </ScrollView>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
@@ -11,7 +11,7 @@ module.exports = {
         light: {
           100: "#D6C6FF",
           200: "#A8B5DB",
-          200: "#9CA4AB"
+          300: "#9CA4AB"
         },
         dark: {
           100: "#221F3D",
