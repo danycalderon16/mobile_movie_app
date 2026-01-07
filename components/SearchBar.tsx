7c9e60abba480d0548e3eb5a72bb417f286372f4
Daniel Calderon <danycalvir@gmail.com>
2026-01-06 23:08:46 -0700

    search movies component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,19 +3,21 @@ import React from 'react';
 import { Image, StyleSheet, TextInput, View } from 'react-native';
 
 interface Pros {
-    onPress: () => void
+    onPress?: () => void
     placeholder: string; 
+    value: string
+    onChangeText: (text:string) => void
 }
 
-const SearchBar = ({onPress, placeholder}:Pros) => {
+const SearchBar = ({onPress, placeholder, value, onChangeText}:Pros) => {
     return (
         <View className=' flex-row items-center bg-dark-200 rounded-full px-3 py-4'>
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff" />
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=""
-                onChange={()=>{}}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor="#a8b5db"
                 className='flex-1 ml-2 text-white'
             />
